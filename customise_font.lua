4814e8f8e88e216c4f0311134ab15e8b9dd3ef6d
AzuredBlue <78235122+AzuredBlue@users.noreply.github.com>
2025-03-02 10:48:20 +0100

    Remove debug, add coomments, improve overall

---
+++ customise_font.lua
@@ -0,0 +1,399 @@
+local mp = require 'mp'
+local utils = require 'mp.utils'
+local msg = require 'mp.msg'
+
+local CONFIG_FILENAME = "customise_font.conf"
+local DEFAULT_PLAYRESX = 640
+local DEFAULT_PLAYRESY = 360
+
+local options = {
+    -- Options
+    debug = true,
+    set_sub_pos = true,
+    only_modify_default_font = true,
+    -- Font sizes for SRT. 44 seems to be the best for me.
+    DEFAULT_FONT_SIZE = 44,
+    SMALL_FONT_SIZE = 44 - 2,
+
+    -- Default style values
+    ass_index = 1,
+    non_ass_index = 1,
+    font_size = DEFAULT_FONT_SIZE,
+    ass_small_font = false,
+    
+    -- Add your own styles here
+    styles = {
+        -- ASS Styles:
+        ass = {
+            "FontName=Netflix Sans,PrimaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H00000000,Bold=-1,Outline=1.3,Shadow=0,Blur=7",
+            "FontName=Gandhi Sans,Bold=1,Outline=1.2,Shadow=0.6666,ShadowX=2,ShadowY=2",
+            "FontName=Trebuchet MS,Bold=1,Outline=1.8,Shadow=1,ShadowX=2,ShadowY=2",
+            -- I recommend leaving this here, so you can always cycle back to default
+            ""
+        },
+        -- Other styles, such as SRT:
+        non_ass = {
+            {
+                name = "Netflix Style",
+                font = "Netflix Sans", 
+                bold = true,
+                blur = 3,
+                border_color = "#000000",
+                border_size = 2,
+                shadow_color = "#000000",
+                shadow_offset = 0
+            },
+            {
+                name = "Gandhi Style",
+                font = "Gandhi Sans",
+                bold = true,
+                blur = 0,
+                border_color = "#000000",
+                border_size = 2.5,
+                shadow_color = "#000000",
+                shadow_offset = 1
+            },
+            {
+                name = "CR",
+                font = "Trebuchet MS",
+                bold = true,
+                blur = 0,
+                border_color = "#000000",
+                border_size = 3,
+                shadow_color = "#000000",
+                shadow_offset = 1.5
+            }
+        }
+    }
+}
+
+local function get_config_path()
+    if package.config:sub(1,1) == '\\' then
+        -- Windows path
+        local appdata = os.getenv("APPDATA")
+        if appdata then
+            return utils.join_path(utils.join_path(appdata, "mpv"), "script-opts")
+        else
+            local home = os.getenv("USERPROFILE")
+            if home then
+                return utils.join_path(
+                    utils.join_path(
+                        utils.join_path(
+                            utils.join_path(home, "AppData"),
+                        "Roaming"),
+                    "mpv"),
+                "script-opts")
+            end
+        end
+    else
+        -- Unix path
+        local home = os.getenv("HOME")
+        if home then
+            return utils.join_path(utils.join_path(home, ".config"), "mpv")
+        end
+    end
+    return nil
+end
+
+local function ensure_config_directory()
+    local config_path = get_config_path()
+    if not config_path then return false end
+    
+    local success, err = utils.file_info(config_path)
+    if not success then
+        msg.verbose("Creating config directory:", config_path)
+        return utils.subprocess({
+            args = {"mkdir", "-p", config_path},
+            cancellable = false
+        })
+    end
+    return true
+end
+
+local function get_playres_scale()
+    local sub_data = mp.get_property("sub-ass-extradata") or ""
+    
+    if sub_data == "" then
+        return 1.0
+    end
+    
+    local playresx = tonumber(sub_data:match("PlayResX:%s*(%d+)")) or DEFAULT_PLAYRESX
+    local playresy = tonumber(sub_data:match("PlayResY:%s*(%d+)")) or DEFAULT_PLAYRESY
+
+    local xRatio = playresx / DEFAULT_PLAYRESX
+    local yRatio = playresy / DEFAULT_PLAYRESY
+    if xRatio >= yRatio then
+        return xRatio
+    else
+        return yRatio
+    end
+end
+
+local function scale_ass_style(style)
+    local scale = get_playres_scale()
+    if scale == 1 then return style end
+    
+    return style:gsub("([%w]+)=([%d%.]+)", function(key, val)
+        local scaled_properties = {
+            Outline = true, Shadow = true, 
+            ShadowX = true, ShadowY = true
+        }
+        
+        if scaled_properties[key] then
+            return string.format("%s=%.1f", key, tonumber(val) * scale)
+        end
+        return string.format("%s=%s", key, val)
+    end)
+end
+
+-- Try to get the 'most popular one'
+local function get_default_font_and_styles(sub_data)
+    local blacklist = {
+        "sign", "song", "^ed", "^op", "title", "^os"
+    }
+    
+    local valid_styles = {}  -- Stores {name, font, size}
+    local font_counts = {}  -- Tracks font+size popularity
+    
+    for line in sub_data:gmatch("[^\r\n]+") do
+        if line:match("Styles%]$") then
+            for style_line in sub_data:gmatch("Style:([^\r\n]+)") do
+                local params = {}
+                for param in style_line:gmatch("([^,]+)") do
+                    table.insert(params, param:match("^%s*(.-)%s*$"))
+                end
+                
+                if #params >= 3 then  -- Need at least 3 params (name, font, size)
+                    local style_name = params[1]
+                    local font_name = params[2]
+                    local font_size = params[3]
+                    local lower_name = style_name:lower()
+
+                    -- Check blacklist patterns
+                    local skip = false
+                    for _, pattern in ipairs(blacklist) do
+                        if lower_name:find(pattern) then
+                            skip = true
+                            break
+                        end
+                    end
+                    
+                    if not skip then
+                        local font_key = font_name .. "|" .. font_size
+                        table.insert(valid_styles, {
+                            name = style_name,
+                            font = font_name,
+                            size = font_size,
+                            key = font_key
+                        })
+                        font_counts[font_key] = (font_counts[font_key] or 0) + 1
+                    end
+                end
+            end
+            break
+        end
+    end
+
+    -- Find most popular font+size combination
+    local max_count, popular_key = 0, nil
+    for key, count in pairs(font_counts) do
+        if count > max_count or (count == max_count and not popular_key) then
+            max_count = count
+            popular_key = key
+        end
+    end
+
+    -- Extract font name and size from popular key
+    local popular_font, popular_size
+    if popular_key then
+        popular_font, popular_size = popular_key:match("([^|]+)|(.+)")
+    end
+
+    -- Collect all styles using the popular font+size
+    local styles = {}
+    if popular_key then
+        for _, style in ipairs(valid_styles) do
+            if style.key == popular_key then
+                table.insert(styles, style.name)
+            end
+        end
+    end
+    
+    return popular_font, popular_size, styles
+end
+
+-- Prefix the style with the style names, so it only changes them.
+local function prefix_style_with_styles(style_names, scaled_style)
+    local all_parts = {}
+    
+    for _, style_name in ipairs(style_names) do
+        local parts = {}
+        for param in scaled_style:gmatch("([^,]+)") do
+            local key, value = param:match("^([^=]+)=(.+)$")
+            if key and value then
+                table.insert(parts, string.format("%s.%s=%s", style_name, key, value))
+            else
+                table.insert(parts, param)
+            end
+        end
+        table.insert(all_parts, table.concat(parts, ","))
+    end
+    
+    return table.concat(all_parts, ",")
+end
+
+local function apply_ass_style()
+    local style = options.styles.ass[options.ass_index]
+    if not style then return end
+    
+    -- Scale the style based on the PlayRes
+    local scaled_style = scale_ass_style(style)
+
+    -- Try to only apply to the default font
+    local sub_data = mp.get_property("sub-ass-extradata", "")
+
+    local default_style_name, default_size, matching_styles = get_default_font_and_styles(sub_data)
+
+    if options.debug and options.only_modify_default_font then
+        local style_list = #matching_styles > 0 and table.concat(matching_styles, ", ") or "none"
+        msg.info(("Replacing font '%s' (size %s) used by %d styles: %s")
+            :format(
+                default_style_name or "nil",
+                default_size or "nil",
+                #matching_styles,
+                style_list
+            ))
+    end
+
+    if options.ass_small_font then
+        scaled_style = scaled_style .. string.format(",FontSize=%d", math.floor((0.95*default_size) + 0.5))
+    end
+
+    if #matching_styles > 0 and options.only_modify_default_font then
+        scaled_style = prefix_style_with_styles(matching_styles, scaled_style)
+    end
+
+    -- Fix LayoutRes
+    local layoutResY = tonumber(sub_data:match("LayoutResY:%s*(%d+)")) or ""
+    local playresy = tonumber(sub_data:match("PlayResY:%s*(%d+)"))
+
+    if layoutResY == "" and playresy < 720 then
+        local playresx = tonumber(sub_data:match("PlayResX:%s*(%d+)"))
+        if playresx ~= "" then
+            local layoutresString = string.format("LayoutResX=%s,LayoutResY=%s", playresx, playresy)
+            scaled_style =  layoutresString .. ',' .. scaled_style
+        end
+    end
+    
+    mp.set_property("sub-ass-style-overrides", scaled_style)
+    
+    if options.set_sub_pos then
+        mp.set_property("sub-pos", 98)
+    end
+end
+
+local function apply_non_ass_style()
+    local style = options.styles.non_ass[options.non_ass_index]
+    if not style then return end
+    
+    mp.set_property_native("sub-font", style.font)
+    mp.set_property_native("sub-bold", style.bold)
+    mp.set_property_native("sub-font-size", options.font_size)
+    mp.set_property_native("sub-blur", style.blur)
+    mp.set_property_native("sub-border-color", style.border_color)
+    mp.set_property_native("sub-border-size", style.border_size)
+    mp.set_property_native("sub-shadow-color", style.shadow_color)
+    mp.set_property_native("sub-shadow-offset", style.shadow_offset)
+
+    if options.set_sub_pos then
+        mp.set_property("sub-pos", 100)
+    end
+    
+end
+
+local function load_config()
+    local config_path = utils.join_path(get_config_path(), CONFIG_FILENAME)
+    local file = io.open(config_path, "r")
+    if not file then return end
+    
+    local content = file:read("*all")
+    file:close()
+    
+    local json_data = utils.parse_json(content)
+    if type(json_data) == "table" then
+        options.ass_index = json_data.ass_index or options.ass_index
+        options.non_ass_index = json_data.non_ass_index or options.non_ass_index
+        options.font_size = json_data.font_size or options.font_size
+        
+        if json_data.ass_small_font ~= nil then
+            options.ass_small_font = json_data.ass_small_font
+        end
+    end
+end
+
+local function save_config()
+    if not ensure_config_directory() then return end
+    
+    local config_path = utils.join_path(get_config_path(), CONFIG_FILENAME)
+    local file = io.open(config_path, "w")
+    if not file then return end
+    
+    local config_to_save = {
+        ass_index = options.ass_index,
+        non_ass_index = options.non_ass_index,
+        font_size = options.font_size,
+        ass_small_font = options.ass_small_font
+    }
+    
+    local json_content = utils.format_json(config_to_save)
+    file:write(json_content)
+    file:close()
+end
+
+local function is_ass_subtitle()
+    local track = mp.get_property_native("current-tracks/sub")
+    return track and track.codec == "ass"
+end
+
+local function toggle_font_size()
+    if is_ass_subtitle() then
+        options.ass_small_font = not options.ass_small_font
+        apply_ass_style()
+        mp.osd_message("ASS Font Size: " .. (options.ass_small_font and "Small" or "Normal"), 2)
+    else
+        options.font_size = options.font_size == options.DEFAULT_FONT_SIZE 
+            and options.SMALL_FONT_SIZE 
+            or options.DEFAULT_FONT_SIZE
+        apply_non_ass_style()
+        mp.osd_message("SRT Font Size: " .. options.font_size, 2)
+    end
+    save_config()
+end
+
+local function cycle_styles(direction)
+    if is_ass_subtitle() then
+        options.ass_index = (options.ass_index + direction - 1) % #options.styles.ass + 1
+        apply_ass_style()
+        mp.osd_message("ASS Style: " .. options.ass_index, 2)
+    else
+        options.non_ass_index = (options.non_ass_index + direction - 1) % #options.styles.non_ass + 1
+        apply_non_ass_style()
+        mp.osd_message("SRT Style: " .. options.styles.non_ass[options.non_ass_index].name, 2)
+    end
+    save_config()
+end
+
+mp.register_event("file-loaded", function()
+    load_config()
+    if is_ass_subtitle() then
+        apply_ass_style()
+    else
+        print(mp.get_property('sub-font-size'))
+        apply_non_ass_style()
+    end
+end)
+
+-- Key Bindings
+mp.add_key_binding("k", "cycle_styles_forward", function() cycle_styles(1) end)
+mp.add_key_binding("K", "cycle_styles_backward", function() cycle_styles(-1) end)
+mp.add_key_binding("Ctrl+k", "toggle_font_size", toggle_font_size)
