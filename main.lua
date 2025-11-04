33457fe192a9acad773f3bfde60bc52424308113
AzuredBlue <78235122+AzuredBlue@users.noreply.github.com>
2025-11-04 15:05:40 +0100

    Allow styles to be loaded/reloaded from another file

--- main.lua
+++ main.lua
@@ -62,90 +62,7 @@ elseif type(options.blacklist) == "string" then
 end
 
 -- Add your own styles here 
-local styles = {
-    -- ASS Styles:
-    ass = {
-        --"FontName=Trebuchet MS,Bold=0,PrimaryColour=&H00FFFFFF,SecondaryColour=&H000000FF,OutlineColour=&H00000000,BackColour=&H00000000,Outline=2,Shadow=1",
-        --"FontName=Cabin,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20",        
-        -- "FontName=Netflix Sans Medium,PrimaryColour=&H00FFFFFF,SecondaryColour=&H000000FF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20",
-        "FontName=LTFinnegan Medium,Bold=0,PrimaryColour=&H00FFFFFF,SecondaryColour=&H000000FF,OutlineColour=&H00000000,BackColour=&H00000000,Outline=1,Shadow=0.23,MarginV=20",
-        "FontName=Gandhi Sans,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20,FontSize=23",         
-        -- "FontName=Gandhi Sans,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00211110,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20",         
-        -- "FontName=Product Sans,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00211110,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20,FontSize=25",        
-        "FontName=Product Sans,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20,FontSize=25",        
-        -- "FontName=Gandhi Sans,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H002B2524,BackColour=&HC0171010,Outline=1.2,Shadow=0.5,MarginV=20",        
-        -- "FontName=Cronos Pro Light,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H000000FF,OutlineColour=&H00000000,BackColour=&HC0000000,Outline=1.4,Shadow=0.8,MarginV=20",
-        "FontName=Cronos Pro Light,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20,FontSize=25",
-        "FontName=Noto Serif,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.45,Shadow=0.75,MarginV=20",
-        -- I recommend leaving this here, so you can always cycle back to default
-        ""
-    },
-    -- Other styles, such as SRT:
-    non_ass = {
-        {
-            name = "LTFinnegan",
-            font = "LTFinnegan Medium",
-            bold = false,
-            blur = 0,
-            border_color = "#000000",
-            border_size = 2.1,
-            shadow_color = "#80000000",
-            shadow_offset = 0.9
-        },
-        -- {
-        --     name = "Netflix Sans",
-        --     font = "Netflix Sans Medium",
-        --     bold = false,
-        --     blur = 0,
-        --     border_color = "#000000",
-        --     border_size = 2.1,
-        --     shadow_color = "#80000000",
-        --     shadow_offset = 0.9
-        -- },
-        {
-            name = "Gandhi Style",
-            font = "Gandhi Sans",
-            bold = true,
-            blur = 0,
-            -- border_color = "#101121",
-            border_color = "#000000",
-            border_size = 2.1,
-            shadow_color = "#80000000",
-            shadow_offset = 0.9
-        },
-        {
-            name = "Product Sans",
-            font = "Product Sans",
-            bold = true,
-            blur = 0,
-            border_color = "#101121",
-            border_size = 2.1,
-            shadow_color = "#80000000",
-            shadow_offset = 0.9
-        },
-        {
-            name = "Cronos Pro",
-            font = "Cronos Pro",
-            bold = true,
-            blur = 0,
-            border_color = "#000000",
-            border_size = 2.1,
-            shadow_color = "#80000000",
-            shadow_offset = 0.9
-        },
-        {
-            name = "Noto Serif",
-            font = "Noto Serif", 
-            bold = true,
-            blur = 0,
-            border_color = "#000000",
-            border_size = 3.6,
-            shadow_color = "#80000000",
-            shadow_offset = 1.1
-        },
-
-    }
-}
+local styles = require("styles")
 
 local script_dir = (debug.getinfo(1).source:match("@?(.*/)") or "./")
 
@@ -636,7 +553,14 @@ mp.register_event("shutdown", function()
     save_config()
 end)
 
+local function reload()
+    package.loaded["styles"] = nil
+    styles = require("styles")
+    cycle_styles(0)
+end
+
 -- Key Bindings
 mp.add_key_binding("k", "cycle_styles_forward", function() cycle_styles(1) end)
 mp.add_key_binding("K", "cycle_styles_backward", function() cycle_styles(-1) end)
 mp.add_key_binding("Ctrl+k", "toggle_font_size", toggle_font_size)
+mp.add_key_binding("Ctrl+r", "reload", reload)
---
+++ styles.lua
@@ -0,0 +1,112 @@
+return {
+    -- ASS Styles:
+    ass = {
+        --"FontName=Trebuchet MS,Bold=0,PrimaryColour=&H00FFFFFF,SecondaryColour=&H000000FF,OutlineColour=&H00000000,BackColour=&H00000000,Outline=2,Shadow=1",
+        "FontName=Netflix Sans Medium,PrimaryColour=&H00FFFFFF,SecondaryColour=&H000000FF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.2,Shadow=0.5,MarginV=20",
+        "FontName=LTFinnegan Medium,Bold=0,PrimaryColour=&H00FFFFFF,SecondaryColour=&H000000FF,OutlineColour=&H00000000,BackColour=&H00000000,Outline=1,Shadow=0.23,MarginV=20",
+        "FontName=Gandhi Sans,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H7F000000,Outline=1.2,Shadow=0.5,MarginV=20",
+        "FontName=SinaNovaW01-Regular,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H002B2524,BackColour=&H80171010,Outline=1.45,Shadow=0.75,MarginV=20,FontSize=27",
+        "FontName=Noto Serif Regular,Bold=1,PrimaryColour=&H00FFFFFF,SecondaryColour=&H00FFFFFF,OutlineColour=&H00000000,BackColour=&H80000000,Outline=1.45,Shadow=0.75,MarginV=20,FontSize=25",
+        -- I recommend leaving this here, so you can always cycle back to default
+        ""
+    },
+    -- Other styles, such as SRT:
+    non_ass = {
+        -- {
+        --     name = "LTFinnegan",
+        --     font = "LTFinnegan Medium",
+        --     bold = false,
+        --     blur = 0,
+        --     border_color = "#000000",
+        --     border_size = 2.1,
+        --     shadow_color = "#80000000",
+        --     shadow_offset = 0.9
+        -- },
+        -- {
+        --     name = "Netflix Sans",
+        --     font = "Netflix Sans Medium",
+        --     bold = false,
+        --     blur = 0,
+        --     border_color = "#000000",
+        --     border_size = 2.1,
+        --     shadow_color = "#80000000",
+        --     shadow_offset = 0.9
+        -- },
+        {
+            name = "Gandhi Style",
+            font = "Gandhi Sans",
+            bold = true,
+            blur = 0,
+            border_color = "#101121",
+            -- border_color = "#000000",
+            border_size = 2.1,
+            shadow_color = "#80000000",
+            shadow_offset = 0.9
+        },
+        {
+            name = "Product Sans",
+            font = "Product Sans",
+            bold = true,
+            blur = 0,
+            border_color = "#101121",
+            border_size = 2.1,
+            shadow_color = "#80000000",
+            shadow_offset = 0.9
+        },
+        -- {
+        --     name = "Cronos Pro",
+        --     font = "Cronos Pro",
+        --     bold = true,
+        --     blur = 0,
+        --     border_color = "#000000",
+        --     border_size = 2.1,
+        --     shadow_color = "#80000000",
+        --     shadow_offset = 0.9
+        -- },
+        {
+            name = "Sina Nova Black",
+            font = "SinaNovaW01-Regular",
+            bold = true,
+            blur = 0,
+            border_color = "#24252b",
+            font_size = 54,
+            border_size = 2.7,
+            shadow_color = "#101016",
+            shadow_offset = 1.6
+        },
+        -- {
+        --     name = "Sina Nova Black",
+        --     font = "SinaNovaW01-Regular",
+        --     bold = true,
+        --     blur = 0,
+        --     border_color = "#101121",
+        --     font_size = 54,
+        --     border_size = 2.7,
+        --     shadow_color = "#80000000",
+        --     shadow_offset = 1.6
+        -- },
+        {
+            name = "Sina Nova",
+            font = "SinaNovaW01-Regular",
+            bold = true,
+            blur = 0,
+            border_color = "#1f2912",
+            font_size = 54,
+            border_size = 3,
+            shadow_color = "#1f2912",
+            shadow_offset = 1.6
+        },
+        {
+            name = "Noto Serif",
+            font = "Noto Serif Regular",
+            bold = true,
+            blur = 0,
+            border_color = "#000000",
+            border_size = 2,7,
+            shadow_color = "#00000080",
+            shadow_offset = 1.6
+        },
+        
+
+    }
+}
